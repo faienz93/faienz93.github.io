11483e228128a82773bfe9a91a5a81d4038140f2
antoniofaienza93 <afaienza93@gmail.com>
2021-01-01 19:46:03 +0100

    update language (TO FINISH)

--- js/language.js
+++ js/language.js
@@ -1,4 +1,5 @@
 $(function() {
+    // https://stackoverflow.com/a/43515426
     $('#toggle-event').change(function() {
        document.body.className = $(this).data($(this).prop("checked").toString());
        console.log(document.body.className);
